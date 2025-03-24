0ae71e4991323efc5960088d09d5c55c0a9c34b8
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-03-24 19:06:27 +0100

    add signup and login

---
+++ controllers/authController.js
@@ -0,0 +1,52 @@
+const jwt = require("jsonwebtoken");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  const token = signToken(user._id);
+
+  res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new AppError("Please provide email and password!", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError("Invalid email or password", 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: "success",
+    token,
+  });
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,65 +1,120 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema(
-  {
-    firstName: {
-      type: String,
-      required: [true, "User must have a firstName"],
-      minlength: [2, "First name must have more or equal 2 characters"],
-    },
-    lastName: {
-      type: String,
-      required: [true, "User must have a lastName"],
-    },
-    email: {
-      type: String,
-      required: [true, "User must have an email"],
-      validate: [validator.isEmail, "Email is incorrect"],
-    },
-    phone: String,
-    birthDate: {
-      type: Date,
-      required: [true, "User must have a birthDate"],
-    },
-    role: {
-      type: String,
-      default: "User",
-      enum: {
-        values: ["User", "Moderator", "Admin"],
-        message: "Role must be either: User, Moderator, Admin",
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [2, "Name must have more or equal 2 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: [
+      true,
+      '"This email is already in use. Please use a different one."',
+    ],
+    lowercase: true,
+    validate: [validator.isEmail, "Email is incorrect"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [8, "Password must have more or equal 8 characters"],
+    select: false,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Please, confirm your password."],
+    validate: {
+      validator: function (el) {
+        return this.password === el;
       },
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now(),
-      select: false,
-    },
-    secretUser: {
-      type: Boolean,
-      default: false,
+      message: "Passwords are not the same",
     },
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
-);
-
-userSchema.virtual("age").get(function () {
-  const today = new Date();
-  const birthDate = new Date(this.birthDate);
-
-  let age = today.getFullYear() - birthDate.getFullYear();
-  if (
-    today <
-    new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate())
-  ) {
-    age--;
-  }
-
-  return age;
 });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+
+  next();
+});
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: [true, "User must have a firstName"],
+//       minlength: [2, "First name must have more or equal 2 characters"],
+//     },
+//     lastName: {
+//       type: String,
+//       required: [true, "User must have a lastName"],
+//     },
+//     email: {
+//       type: String,
+//       required: [true, "User must have an email"],
+//       validate: [validator.isEmail, "Email is incorrect"],
+//     },
+//     phone: String,
+//     birthDate: {
+//       type: Date,
+//       required: [true, "User must have a birthDate"],
+//     },
+//     role: {
+//       type: String,
+//       default: "User",
+//       enum: {
+//         values: ["User", "Moderator", "Admin"],
+//         message: "Role must be either: User, Moderator, Admin",
+//       },
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now(),
+//       select: false,
+//     },
+//     secretUser: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   {
+//     toJSON: { virtuals: true },
+//     toObject: { virtuals: true },
+//   }
+// );
+
+// userSchema.virtual("age").get(function () {
+//   const today = new Date();
+//   const birthDate = new Date(this.birthDate);
+
+//   let age = today.getFullYear() - birthDate.getFullYear();
+//   if (
+//     today <
+//     new Date(today.getFullYear(), birthDate.getMonth(), birthDate.getDate())
+//   ) {
+//     age--;
+//   }
+
+//   return age;
+// });
 
 // userSchema.pre("save", function (next) {
 //   console.log(this);
@@ -81,6 +136,6 @@ userSchema.virtual("age").get(function () {
 //   next();
 // });
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+// module.exports = User;
