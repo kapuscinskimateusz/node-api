f3d9a299b6ab5c2e136c77b5fd521935d9a4eb24
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-03-31 00:31:42 +0200

    add updateMe and deleteMe routes

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,6 +3,49 @@ const APIFeatures = require("./../utils/apiFeatures");
 const AppError = require("./../utils/appError");
 const catchAsync = require("./../utils/catchAsync");
 
+const filterObj = (obj, ...fields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (fields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        "This isn't route for updating password. Please use /updateMyPassword.",
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, "name", "email");
+  const user = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      user,
+    },
+  });
+});
+
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user._id, { active: false });
+
+  res.status(204).json({
+    status: "success",
+    data: null,
+  });
+});
+
 exports.aliasYoungestUsers = (req, res, next) => {
   req.query.limit = "3";
   req.query.sort = "age";
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,9 @@ router.patch(
   authController.updateMyPassword
 );
 
+router.patch("/updateMe", authController.protect, userController.updateMe);
+router.delete("/deleteMe", authController.protect, userController.deleteMe);
+
 router
   .route("/youngest-3")
   .get(userController.aliasYoungestUsers, userController.getAllUsers);
