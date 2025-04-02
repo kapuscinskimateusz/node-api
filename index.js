941b35a4f2ba03cefc02f953a8f079a5daaef3f2
Mateusz <kapuscinski.mateusz01@gmail.com>
2025-04-02 19:10:24 +0200

    set security HTTP headers, add data sanitization

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
 const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,10 +11,15 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
+// Set security HTTP headers
+app.use(helmet());
+
+// Drvelopment logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -19,13 +27,24 @@ const limiter = rateLimit({
 });
 app.use("/api", limiter);
 
-app.use(express.json());
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against noSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Serving static files
 app.use(express.static("public"));
 
-// app.use((req, res, next) => {
-//   req.requestTime = new Date().toISOString();
-//   next();
-// });
+// Test middleware
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+
+  next();
+});
 
 app.use("/api/v1/users", userRouter);
 
